2c5b2ae1fda530c6cc087ecea0efc6f022338ded
Jiri Kvasnicka <jiri.kvasnicka@desy.de>
2018-03-21 16:35:44 +0100

    hello world in perl

--- spiroc2e-sc.pl
+++ spiroc2e-sc.pl
@@ -1,3 +1,8 @@
 #use warnings
-$baseSrcDir="/home/kvas/ahcal/slowcontrols/reference"
+$baseSrcDir = "/home/kvas/ahcal/slowcontrols/reference";
 
+my @list_dirs = grep { -d } glob "$baseSrcDir/*";
+
+foreach (@list_dirs) {
+	print $_. "\n";
+}
