2b17126a43028e7ec93b9359263712f2eb67a95c
jkvas <jiri.kvasnicka@desy.de>
2018-03-23 21:46:32 +0100

    bit array added

--- spiroc2e-sc.pl
+++ spiroc2e-sc.pl
@@ -1,7 +1,9 @@
+#!/usr/bin/perl
 use strict;
 use warnings;
 
-my $baseSrcDir        = "/home/kvas/pool/SPIROC2E-SC/reference";
+#my $baseSrcDir        = "/home/kvas/pool/SPIROC2E-SC/reference";
+my $baseSrcDir        = "./reference";
 my $dirNames          = "Module";
 my $suffix            = "AT";
 my $selection_modules = "18";
@@ -21,54 +23,66 @@ my $slabNo;
 my @asic_files;
 my $asic_file;
 my $asicIndex;
-MODULES:
 
-foreach $dir (@list_dirs) {
+MODULES:
+    foreach $dir (@list_dirs) {
 	($moduleNo) = $dir =~ /\D(\d+)$/;
 	grep( /^$moduleNo$/, split( /,/, $selection_modules ) ) || next MODULES;
 	($suffix_dir) = ( grep( -d && /\/$suffix$/, glob "$dir/*" ) );
 	@slab_dirs = grep ( -d && /\/slab\d$/, glob "$suffix_dir/*" );
-  SLABS:
+      SLABS:
 	foreach my $slab_dir (@slab_dirs) {
-		($slabNo) = $slab_dir =~ /slab(\d)$/;
-		grep( /^$slabNo$/, split( /,/, $selection_slabs ) ) || next SLABS;
-		@asic_files = grep ( -T && /SC_SP2b_ASIC\d+.txt/, glob "$slab_dir/*" );
+	    ($slabNo) = $slab_dir =~ /slab(\d)$/;
+	    grep( /^$slabNo$/, split( /,/, $selection_slabs ) ) || next SLABS;
+	    @asic_files = grep ( -T && /SC_SP2b_ASIC\d+.txt/, glob "$slab_dir/*" );
 	  ASICS:
-		foreach $asic_file (@asic_files) {
-			$asicIndex = ( $asic_file =~ /ASIC(\d+).txt/ )[0];
-			grep( /^$asicIndex$/, split( /,/, $selection_asics ) )
-			  || next ASICS;
-			push( @scfiles, $asic_file );
-		}
+	    foreach $asic_file (@asic_files) {
+		$asicIndex = ( $asic_file =~ /ASIC(\d+).txt/ )[0];
+		grep( /^$asicIndex$/, split( /,/, $selection_asics ) )
+		    || next ASICS;
+		push( @scfiles, $asic_file );
+	    }
 	}
 }
 
+my @spiroc_sc = ();#the slowcontrol bit array
 foreach my $file (@scfiles) {
-	load_sc($file);
+    @spiroc_sc  =();
+    load_sc($file);
 }
+
 
 sub load_sc {
-	my $filename = $_[0];
-	print "#", $_[0], "\n";
-	my @spiroc_sc = ();
-	local $/ = undef;
-	open( my $FILE, $filename );
-	my $content = <$FILE>;
-	my @lines = split /\r\n|\n|\r/, $content;
-	foreach my $line (@lines) {
-		print $line, "\t(";
-		for my $i ( 0 .. 7 ) {
-			if ( $line & ( 1 << ( 7 - $i ) ) ) {
-				print "1";
-			}
-			else {
-				print "0";
-			}
-		}
-		print "),\n";
+    my $filename = $_[0];
+    print "#", $_[0], "\n";
+    local $/ = undef;
+    open( my $FILE, $filename );
+    my $content = <$FILE>;
+    my @lines = split /\r\n|\n|\r/, $content;
+    my $bitindex=0;
+    print "size=:",$#spiroc_sc,"\n";
+    foreach my $line (@lines) {
+	$line =~ /^\d+/ || next;
+	print $line, "(";
+	for my $i ( 0 .. 7 ) {
+	    if ( $line & ( 1 << ( 7 - $i ) ) ) {
+		print "1";
+		$spiroc_sc[$bitindex++] = 1;
+	    }
+	    else {
+		print "0";
+		$spiroc_sc[$bitindex++] = 0;
+	    }
 	}
-
-	#	print "#", $_[0], "\n";
-	return;
+	print "), ";
+    }
+    print "\n";
+    print "size=:",$#spiroc_sc,"\n";
+    @spiroc_sc = @spiroc_sc[0..($#spiroc_sc - 7)];
+    print "size=:",$#spiroc_sc,"\n";
+    @spiroc_sc = reverse @spiroc_sc;
+    print "size=:",$#spiroc_sc,"\n";
+    #	print "#", $_[0], "\n";
+    return;
 }
 
