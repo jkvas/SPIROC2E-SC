c47d6c3bf0319dbfcaae1cc8df1ae0ca0c8c7a69
Jiri Kvasnicka <jiri.kvasnicka@desy.de>
2018-03-23 17:56:44 +0100

    binary convertor in progress

--- spiroc2e-sc.pl
+++ spiroc2e-sc.pl
@@ -1,24 +1,35 @@
-#use warnings
+use strict;
+use warnings;
+
 my $baseSrcDir        = "/home/kvas/pool/SPIROC2E-SC/reference";
 my $dirNames          = "Module";
 my $suffix            = "AT";
-my $selection_modules = "1";
-my $selection_slabs   = "2";
+my $selection_modules = "18";
+my $selection_slabs   = "2,3";
 
 #my $selection_asics   = "01,02,03,04,04,05,06,07,08";
-my $selection_asics = "08";
+my $selection_asics = "01,02";
 
 my @list_dirs = grep ( -d && /$dirNames/, glob "$baseSrcDir/*" );
 my @module_dirs;
 my @scfiles = ();
+my $dir;
+my $moduleNo;
+my @slab_dirs;
+my $suffix_dir;
+my $slabNo;
+my @asic_files;
+my $asic_file;
+my $asicIndex;
 MODULES:
+
 foreach $dir (@list_dirs) {
 	($moduleNo) = $dir =~ /\D(\d+)$/;
 	grep( /^$moduleNo$/, split( /,/, $selection_modules ) ) || next MODULES;
 	($suffix_dir) = ( grep( -d && /\/$suffix$/, glob "$dir/*" ) );
 	@slab_dirs = grep ( -d && /\/slab\d$/, glob "$suffix_dir/*" );
   SLABS:
-	foreach $slab_dir (@slab_dirs) {
+	foreach my $slab_dir (@slab_dirs) {
 		($slabNo) = $slab_dir =~ /slab(\d)$/;
 		grep( /^$slabNo$/, split( /,/, $selection_slabs ) ) || next SLABS;
 		@asic_files = grep ( -T && /SC_SP2b_ASIC\d+.txt/, glob "$slab_dir/*" );
@@ -32,11 +43,32 @@ foreach $dir (@list_dirs) {
 	}
 }
 
-foreach $file (@scfiles) {
+foreach my $file (@scfiles) {
 	load_sc($file);
 }
 
 sub load_sc {
-	print $_[0], "\n";
+	my $filename = $_[0];
+	print "#", $_[0], "\n";
+	my @spiroc_sc = ();
+	local $/ = undef;
+	open( my $FILE, $filename );
+	my $content = <$FILE>;
+	my @lines = split /\r\n|\n|\r/, $content;
+	foreach my $line (@lines) {
+		print $line, "\t(";
+		for my $i ( 0 .. 7 ) {
+			if ( $line & ( 1 << ( 7 - $i ) ) ) {
+				print "1";
+			}
+			else {
+				print "0";
+			}
+		}
+		print "),\n";
+	}
+
+	#	print "#", $_[0], "\n";
 	return;
 }
+
